aeb7e4f94296dc7e707ff7199811faee9de60ff8
Mohammad <abrar.discloses@gmail.com>
2019-03-18 16:55:46 +0100

    added crab submitter

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh;
 
-export CMSSW_GIT_REFERENCE=/afs/cern.ch/cms/git-cmssw-mirror/cmssw.git3 
+export CMSSW_GIT_REFERENCE=/afs/cern.ch/cms/git-cmssw-mirror/cmssw.git
 
 scramv1 project CMSSW CMSSW_9_4_13
 cd CMSSW_9_4_13/src 
